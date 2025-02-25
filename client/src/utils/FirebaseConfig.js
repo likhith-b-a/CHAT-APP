0ac148404dbdc751d04815604e65e01192331729
likhith-b-a <likhithba51@gmail.com>
2025-02-26 01:31:50 +0530

    Update FirebaseConfig.js

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACUqpoXxn9OreSsaEEk0kgsvK1FWj8nwk",
-  authDomain: "chat-application-6f039.firebaseapp.com",
-  projectId: "chat-application-6f039",
-  storageBucket: "chat-application-6f039.firebasestorage.app",
-  messagingSenderId: "267701263769",
-  appId: "1:267701263769:web:fe60bfecdb722c70604708",
-  measurementId: "G-S8E4984EX1",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
