94956b79319cf856c82f8ef6053eeb174ccd567d
likhith-b-a <likhithba51@gmail.com>
2025-02-26 17:50:01 +0530

    Made necessary updates for audio message

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,12 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 export const uploadOnCloudinary = async (fileBuffer) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
       { resource_type: "auto", folder: "CHAT APP" },
