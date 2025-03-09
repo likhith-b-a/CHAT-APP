87a3558e255e46f70c48dd9f8098df65f4f6c6f7
likhith-b-a <likhithba51@gmail.com>
2025-03-09 12:37:25 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_ZEGO_APP_ID: 2035795994,
-    NEXT_PUBLIC_ZEGO_SERVER_SECRET: "7881020ba9d960ae68d6d4d483a2b3bb",
-  },
   images: {
-    domains: ["imgv3.fotor.com", "localhost", "res.cloudinary.com"],
+    domains: ["localhost", "res.cloudinary.com"],
   },
 };
 
